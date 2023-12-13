Try to make fixed Q bridge work for a bit higher dim (not entirely, need to use certain kernel), need to choose the normalization of loss function and optimize the code performance.

sdebridge/diffusion_bridge.py
@@ -3,9 +3,9 @@ from functools import partial
 import jax
 import jax.numpy as jnp
 import tensorflow as tf
-import utils
 from tqdm.notebook import tqdm
 
+from . import utils
 from .networks import ScoreNet
 
 
@@ -26,6 +26,7 @@ class DiffusionBridge:
         self.d = dim
         self.T = end_time
         self.N = num_steps
+
         self.ts = jnp.linspace(0.0, self.T, self.N + 1)
         self.rng = rng
         self.true_score_transition = true_score_transition
@@ -149,8 +150,8 @@ class DiffusionBridge:
 
         Args:
             score_p_state (TrainState): s_{theta}(t, x)
-            initial_condition (jnp.ndarray): Z*(0) = X(T-t)
-            terminal_condition (jnp.ndarray): Z*(T) = X(0)
+            initial_condition (jnp.ndarray): Z*(T) = X(0)
+            terminal_condition (jnp.ndarray): Z*(0) = X(T-t)
             using_true_score (bool): If True, use the predefined score transition function.
 
         Returns:

sdebridge/networks.py
@@ -1,7 +1,13 @@
 import jax.numpy as jnp
 from flax import linen as nn
 
-def get_timestep_embedding(timesteps: jnp.ndarray, embedding_dim: int, max_period: int=10000, scaling_factor: float=100.0) -> jnp.ndarray:
+
+def get_timestep_embedding(
+    timesteps: jnp.ndarray,
+    embedding_dim: int,
+    max_period: int = 10000,
+    scaling_factor: float = 100.0,
+) -> jnp.ndarray:
     assert len(timesteps.shape) == 2 and timesteps.shape[-1] == 1
     half_dim = embedding_dim // 2
     emb = jnp.log(max_period) / (half_dim - 1)
@@ -11,17 +17,23 @@ def get_timestep_embedding(timesteps: jnp.ndarray, embedding_dim: int, max_perio
     assert emb.shape == (timesteps.shape[0], embedding_dim)
     return emb
 
+
 def get_activation(name: str) -> nn.activation:
-    if name == 'relu':
+    if name == "relu":
         return nn.relu
-    elif name == 'leaky_relu':
+    elif name == "leaky_relu":
         return nn.leaky_relu
-    elif name == 'tanh':
+    elif name == "elu":
+        return nn.elu
+    elif name == "gelu":
+        return nn.gelu
+    elif name == "tanh":
         return nn.tanh
-    elif name == 'sigmoid':
+    elif name == "sigmoid":
         return nn.sigmoid
     else:
-        raise ValueError(f'Activation {name} not recognized.')
+        raise ValueError(f"Activation {name} not recognized.")
+
 
 class MLP(nn.Module):
     out_dim: int
@@ -41,7 +53,8 @@ class MLP(nn.Module):
         else:
             x = nn.Dense(self.out_dim)(x)
         return x
-    
+
+
 class ScoreNet(nn.Module):
     out_dim: int
     time_embedding_dim: int
@@ -55,21 +68,27 @@ class ScoreNet(nn.Module):
     def __call__(self, x: jnp.ndarray, t: jnp.ndarray, train: bool) -> jnp.ndarray:
         assert len(x.shape) == len(t.shape) == 2
         t = get_timestep_embedding(t, self.time_embedding_dim)
-        t = MLP(out_dim=self.embedding_dim,
-                act=self.act,
-                layer_dims=self.encoder_layer_dims,
-                apply_act_at_output=False,
-                using_batchnorm=self.using_batchnorm)(t, train)
-        x = MLP(out_dim=self.embedding_dim,
-                act=self.act,
-                layer_dims=self.encoder_layer_dims,
-                apply_act_at_output=False,
-                using_batchnorm=self.using_batchnorm)(x, train)
+        t = MLP(
+            out_dim=self.embedding_dim,
+            act=self.act,
+            layer_dims=self.encoder_layer_dims,
+            apply_act_at_output=False,
+            using_batchnorm=self.using_batchnorm,
+        )(t, train)
+        x = MLP(
+            out_dim=self.embedding_dim,
+            act=self.act,
+            layer_dims=self.encoder_layer_dims,
+            apply_act_at_output=False,
+            using_batchnorm=self.using_batchnorm,
+        )(x, train)
         assert t.shape == x.shape == (x.shape[0], self.embedding_dim)
         xt = jnp.concatenate([x, t], axis=-1)
-        score = MLP(out_dim=self.out_dim,
-                    act=self.act,
-                    layer_dims=self.decoder_layer_dims,
-                    apply_act_at_output=False,
-                    using_batchnorm=self.using_batchnorm)(xt, train)
+        score = MLP(
+            out_dim=self.out_dim,
+            act=self.act,
+            layer_dims=self.decoder_layer_dims,
+            apply_act_at_output=False,
+            using_batchnorm=self.using_batchnorm,
+        )(xt, train)
         return score

sdebridge/utils.py
@@ -1,50 +1,59 @@
 from functools import partial
+
 import jax
 import jax.numpy as jnp
-from flax.training import train_state
-from flax import struct
-from flax import linen as nn
+import matplotlib.pyplot as plt
 import optax
-from clu import metrics
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import matplotlib.pyplot as plt
+from clu import metrics
+from flax import linen as nn
+from flax import struct
+from flax.training import train_state
+
 
 ### Dimension helpers
 def flatten_batch(x: jnp.ndarray):
     assert len(x.shape) >= 2
     return x.reshape(-1, x.shape[-1])
 
+
 def unsqueeze(x: jnp.ndarray, axis: int):
     return jnp.expand_dims(x, axis=axis)
+
 
 def squeeze(x: jnp.ndarray, axis: int):
     return jnp.squeeze(x, axis=axis)
 
+
 def sb_multi(a: jnp.ndarray, b: jnp.ndarray) -> jnp.ndarray:
-    """ Perform matrix-vector multiplication for a single matrix/vector and each matrix/vector in a batch.
-    """
+    """Perform matrix-vector multiplication for a single matrix/vector and each matrix/vector in a batch."""
     return jax.vmap(lambda x, y: jnp.dot(x, y), in_axes=(None, 0), out_axes=0)(a, b)
 
+
 def bb_multi(a: jnp.ndarray, b: jnp.ndarray) -> jnp.ndarray:
-    """ Perform matrix_vector multiplication for each pair of matrix/vector and matrix/vector in two batches.
-    """
+    """Perform matrix_vector multiplication for each pair of matrix/vector and matrix/vector in two batches."""
     return jax.vmap(lambda x, y: jnp.dot(x, y), in_axes=(0, 0), out_axes=0)(a, b)
+
 
 ### Network helpers
 @struct.dataclass
 class Metrics(metrics.Collection):
-    loss: metrics.Average.from_output('loss')
+    loss: metrics.Average.from_output("loss")
+
 
 class TrainState(train_state.TrainState):
     batch_stats: any
     metrics: Metrics
 
-def create_train_state(module: nn.Module, rng: jnp.ndarray, learning_rate: float, input_shapes: list) -> TrainState:
+
+def create_train_state(
+    module: nn.Module, rng: jnp.ndarray, learning_rate: float, input_shapes: list
+) -> TrainState:
     init_inputs = [jnp.zeros(shape=shape) for shape in input_shapes]
     variables = module.init(rng, *init_inputs, train=False)
-    params = variables['params']
-    batch_stats = variables['batch_stats'] if 'batch_stats' in variables else {}
+    params = variables["params"]
+    batch_stats = variables["batch_stats"] if "batch_stats" in variables else {}
 
     tx = optax.adam(learning_rate)
     return TrainState.create(
@@ -52,39 +61,49 @@ def create_train_state(module: nn.Module, rng: jnp.ndarray, learning_rate: float
         params=params,
         tx=tx,
         batch_stats=batch_stats,
-        metrics=Metrics.empty()
+        metrics=Metrics.empty(),
     )
+
 
 @jax.jit
 def eval_score(state: TrainState, x: jnp.ndarray, t: float) -> jnp.ndarray:
-    assert len(x.shape) == 2                                        # (B, d)                                                       
-    t = jnp.tile(t, reps=(x.shape[0], 1))                           # (B, 1)
-    return state.apply_fn({'params': state.params, 'batch_stats': state.batch_stats}, x, t, train=False)
+    assert len(x.shape) == 2  # (B, d)
+    t = jnp.tile(t, reps=(x.shape[0], 1))  # (B, 1)
+    return state.apply_fn(
+        {"params": state.params, "batch_stats": state.batch_stats}, x, t, train=False
+    )
+
 
 def get_iterable_dataset(generator: callable, dtype: any, shape: any):
     if type(dtype) == tf.DType and type(shape) == list:
-        dataset = tf.data.Dataset.from_generator(generator,
-                                                output_signature=(tf.TensorSpec(shape=shape, dtype=dtype)))
+        dataset = tf.data.Dataset.from_generator(
+            generator, output_signature=(tf.TensorSpec(shape=shape, dtype=dtype))
+        )
     elif type(dtype) == tuple and type(shape) == list:
         assert len(dtype) == len(shape)
-        signatures = tuple([tf.TensorSpec(shape=shape[i], dtype=dtype[i]) for i in range(len(dtype))])
+        signatures = tuple(
+            [tf.TensorSpec(shape=shape[i], dtype=dtype[i]) for i in range(len(dtype))]
+        )
         dataset = tf.data.Dataset.from_generator(generator, output_signature=signatures)
     else:
         raise ValueError("Invalid dtype or shape")
     iterable_dataset = iter(tfds.as_numpy(dataset))
     return iterable_dataset
 
+
 ### Plotting helpers
-def plot_2d_vector_field(X: callable, 
-                         X_ref: callable,
-                         xs: jnp.ndarray,
-                         ts: jnp.ndarray,
-                         suptitle: str,
-                         scale: float=None,
-                         **kwargs):
+def plot_2d_vector_field(
+    X: callable,
+    X_ref: callable,
+    xs: jnp.ndarray,
+    ts: jnp.ndarray,
+    suptitle: str,
+    scale: float = None,
+    **kwargs,
+):
     xm, ym = jnp.meshgrid(xs, xs)
     x = jnp.stack([xm.flatten(), ym.flatten()], axis=-1)
-    fig, ax = plt.subplots(1, len(ts), figsize=(4*len(ts), 4))
+    fig, ax = plt.subplots(1, len(ts), figsize=(4 * len(ts), 4))
     X_ref = partial(X_ref, **kwargs) if X_ref is not None else None
     for i, t in enumerate(ts):
         vector_field = X(x, t) if X is not None else None
@@ -92,42 +111,123 @@ def plot_2d_vector_field(X: callable,
         if vector_field is not None:
             u = vector_field[:, 0].reshape(xm.shape)
             v = vector_field[:, 1].reshape(xm.shape)
-            ax[i].quiver(xm, ym, u, v, color='b', scale=scale)
+            ax[i].quiver(xm, ym, u, v, color="b", scale=scale)
 
         if vector_field_ref is not None:
             u_ref = vector_field_ref[:, 0].reshape(xm.shape)
             v_ref = vector_field_ref[:, 1].reshape(xm.shape)
-            ax[i].quiver(xm, ym, u_ref, v_ref, color='r', scale=scale)
-        
+            ax[i].quiver(xm, ym, u_ref, v_ref, color="r", scale=scale)
+
         ax[i].set_title(f"t = {t:.1f}")
     fig.suptitle(suptitle)
-    plt.show() 
+    plt.show()
+
 
 def plot_2d_trajectories(trajectories: jnp.ndarray, title: str, **kwargs):
-    colormap = plt.cm.get_cmap('spring')
+    colormap = plt.cm.get_cmap("spring")
     num_trajectories = trajectories.shape[0]
     colors = [colormap(i) for i in jnp.linspace(0, 1, num_trajectories)]
     for i in range(num_trajectories):
-        plt.plot(trajectories[i, :, 0], trajectories[i, :, 1], color=colors[i], zorder=1, alpha=0.5, **kwargs)
-        plt.scatter(trajectories[i, 1, 0], trajectories[i, 1, 1], color='b', marker='o', edgecolors='k', zorder=2)
-        plt.scatter(trajectories[i, -2, 0], trajectories[i, -2, 1], color='c', marker='D', edgecolors='k', zorder=2)
+        plt.plot(
+            trajectories[i, :, 0],
+            trajectories[i, :, 1],
+            color=colors[i],
+            zorder=1,
+            alpha=0.5,
+            **kwargs,
+        )
+        plt.scatter(
+            trajectories[i, 1, 0],
+            trajectories[i, 1, 1],
+            color="b",
+            marker="o",
+            edgecolors="k",
+            zorder=2,
+        )
+        plt.scatter(
+            trajectories[i, -2, 0],
+            trajectories[i, -2, 1],
+            color="c",
+            marker="D",
+            edgecolors="k",
+            zorder=2,
+        )
     plt.title(title)
 
+
 def plot_trajectories(trajectories: jnp.ndarray, title: str, **kwargs):
-    colormap = plt.cm.get_cmap('spring')
+    colormap = plt.cm.get_cmap("spring")
     assert len(trajectories.shape) == 3
     num_trajectories = trajectories.shape[0]
     dim = trajectories.shape[2]
     colors = [colormap(i) for i in jnp.linspace(0, 1, num_trajectories)]
     plt.figure(figsize=(8, 8))
     for i in range(num_trajectories):
-        for j in range(dim//2):
-            plt.plot(trajectories[i, :, 2*j], trajectories[i, :, 2*j+1], color=colors[i], zorder=1, alpha=0.2, **kwargs)
-            plt.scatter(trajectories[i, 1, 2*j], trajectories[i, 1, 2*j+1], color='b', marker='o', edgecolors='k', zorder=2)
-            plt.scatter(trajectories[i, -2, 2*j], trajectories[i, -2, 2*j+1], color=colors[i], marker='D', edgecolors='k', zorder=2)
+        for j in range(dim // 2):
+            plt.plot(
+                trajectories[i, :, 2 * j],
+                trajectories[i, :, 2 * j + 1],
+                color=colors[i],
+                zorder=1,
+                alpha=0.2,
+                **kwargs,
+            )
+            plt.scatter(
+                trajectories[i, 0, 2 * j],
+                trajectories[i, 0, 2 * j + 1],
+                color="b",
+                marker="o",
+                edgecolors="k",
+                zorder=2,
+            )
+            plt.scatter(
+                trajectories[i, -1, 2 * j],
+                trajectories[i, -1, 2 * j + 1],
+                color=colors[i],
+                marker="D",
+                edgecolors="k",
+                zorder=2,
+            )
     plt.xlim(-2.0, 2.0)
     plt.ylim(-2.0, 2.0)
     plt.title(title)
+
+
+def plot_single_trajectory(trajectory: jnp.ndarray, title: str, **kwargs):
+    colormap = plt.cm.get_cmap("jet")
+    assert len(trajectory.shape) == 2
+    dim = trajectory.shape[-1]
+    colors = [colormap(i) for i in jnp.linspace(0, 1, dim // 2)]
+    plt.figure(figsize=(8, 8))
+    for i in range(dim // 2):
+        plt.plot(
+            trajectory[:, 2 * i],
+            trajectory[:, 2 * i + 1],
+            color=colors[i],
+            zorder=1,
+            alpha=0.2,
+            **kwargs,
+        )
+        plt.scatter(
+            trajectory[0, 2 * i],
+            trajectory[0, 2 * i + 1],
+            color=colors[i],
+            marker="o",
+            edgecolors="k",
+            zorder=2,
+        )
+        plt.scatter(
+            trajectory[-1, 2 * i],
+            trajectory[-1, 2 * i + 1],
+            color=colors[i],
+            marker="D",
+            edgecolors="k",
+            zorder=2,
+        )
+    plt.xlim(-2.0, 2.0)
+    plt.ylim(-2.0, 2.0)
+    plt.title(title)
+
 
 ### Data helpers
 def sample_circle(num_points: int, scale: float, shifts: jnp.ndarray) -> jnp.ndarray:
@@ -135,6 +235,7 @@ def sample_circle(num_points: int, scale: float, shifts: jnp.ndarray) -> jnp.nda
     x = jnp.cos(theta)
     y = jnp.sin(theta)
     return (scale * jnp.stack([x, y], axis=1) + shifts).flatten()
+
 
 def sample_square(num_points: int, scale: float, shifts: jnp.ndarray) -> jnp.ndarray:
     num_points_per_side = num_points // 4