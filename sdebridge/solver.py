7128df18e6df45648af54ae8ebe00c1cc128eb1b
Libby Baker <elba@di.ku.dk>
2024-05-11 09:43:58 +0200

    Euler maruyama now only returns the trajectories, so that matrix inversion only happens when necesary

--- sdebridge/solver.py
+++ sdebridge/solver.py
@@ -12,8 +12,53 @@ def batch_matmul(A: jnp.ndarray, B: jnp.ndarray) -> jnp.ndarray:
     return jax.vmap(jnp.matmul, in_axes=(0, 0), out_axes=0)(A, B)
 
 
-@partial(jax.jit, static_argnums=(0,))
-def euler_maruyama(
+def euler_maruyama(key, x0, ts, drift, diffusion, bm_shape=None):
+    if bm_shape is None:
+        bm_shape = x0.shape
+
+    def step_fun(key_and_x_and_t, dt):
+        k, x, t = key_and_x_and_t
+        k, subkey = jax.random.split(k, num=2)
+        eps = jax.random.normal(subkey, shape=bm_shape)
+        diffusion_ = diffusion(x, t)
+        xnew = x + dt * drift(x, t) + jnp.sqrt(dt) * diffusion_ @ eps
+        tnew = t + dt
+
+        return (k, xnew, tnew), xnew
+
+    init = (key, x0, ts[0])
+    _, x_all = jax.lax.scan(step_fun, xs=jnp.diff(ts), init=init)
+    return jnp.concatenate([x0[None], x_all], axis=0)
+
+
+def gradients_and_covariances(xs, ts, drift, diffusion):
+    @jax.jit
+    def grad_and_cov(t0: float, X0: jax.Array, t1: float, X1: jax.Array):
+        dt = t1 - t0
+        drift_last = drift(X0, t0)
+        diffusion_last = diffusion(X0, t0)
+        cov = diffusion_last @ diffusion_last.T
+        inv_cov = invert(diffusion_last, diffusion_last.T)
+        grad = 1 / dt * inv_cov @ (X1 - X0 - dt * drift_last)
+        return -grad, cov
+
+    grad_cov_fn = jax.vmap(grad_and_cov, in_axes=(0, 0, 0, 0))
+    mult_trajectories = jax.vmap(grad_cov_fn, in_axes=(None, 0, None, 0))
+    return mult_trajectories(ts[:-1], xs[:, :-1], ts[1:], xs[:, 1:])
+
+
+def invert(mat, mat_transpose):
+    """
+    Inversion of mat*mat_transpose.
+    :param mat: array of shape (n, m) i.e. ndim=2
+    :param mat_transpose: array with shape (m, n) with ndim=2
+    :return: (mat*mat.T)^{-1} with shape (n, n)
+    """
+    return jnp.linalg.inv(mat @ mat_transpose)
+
+
+# @partial(jax.jit, static_argnums=(0,))
+def euler_maruyama2(
     sde: SDE,
     initial_vals: jnp.ndarray,
     terminal_vals: jnp.ndarray,
@@ -32,12 +77,11 @@ def euler_maruyama(
         grads=jnp.empty_like(initial_vals),
         covs=jnp.empty(
             (initial_vals.shape[0], sde.dim * sde.n_bases, sde.dim * sde.n_bases),
-            dtype=jnp.complex64,
         ),
         step_key=rng_key,
     )
 
-    @partial(jax.jit, static_argnums=(0, 1))
+    # @partial(jax.jit, static_argnums=(0, 1))
     def euler_maruyama_step(state: SolverState, time: jnp.ndarray) -> tuple:
         """Euler-Maruyama step, NOTE: all the calculations are over batches"""
         time = jnp.expand_dims(time, axis=-1)
@@ -48,7 +92,7 @@ def euler_maruyama(
 
         n_batches = state.vals.shape[0]
         _brownian = jax.random.normal(
-            step_key, shape=(n_batches, sde.dim * sde.n_grid**2)
+            step_key, shape=(n_batches, sde.bm_shape)
         )  # (B, 2*n_grid**2)
         brownian_step = _brownian * jnp.sqrt(sde.dt)
 
@@ -62,6 +106,12 @@ def euler_maruyama(
         )  # (B, 2*n_bases, 2*n_bases)
         _inv_covariance = jax.vmap(
             partial(jnp.linalg.pinv, hermitian=True, rcond=None)
+        )(
+            _covariance
+        )  # (B, 2*n_bases, 2*n_bases)
+
+        _inv_covariance = jax.vmap(
+            partial(jnp.linalg.pinv, hermitian=False, rcond=None)
         )(
             _covariance
         )  # (B, 2*n_bases, 2*n_bases)
@@ -87,7 +137,7 @@ def euler_maruyama(
     _, (trajectories, gradients, covariances, step_keys) = jax.lax.scan(
         euler_maruyama_step,
         init=init_state,
-        xs=(sde.ts[:-1]),
+        xs=(sde.ts),
         length=sde.N,
     )
 
