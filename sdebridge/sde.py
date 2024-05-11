921bbe42ef19ffa56342263d9897d712670b8124
Libby Baker <elba@di.ku.dk>
2024-05-11 09:42:28 +0200

    Made dt, ts properties (should only be based on T and N) sde has bm_shape parameter bridge and reverse are now not class functions so that they don't have to be reinstantiated every time they're called

--- sdebridge/sde.py
+++ sdebridge/sde.py
@@ -1,4 +1,3 @@
-import abc
 import dataclasses
 from functools import partial
 from typing import Any, Callable
@@ -7,53 +6,64 @@ import jax
 import jax.numpy as jnp
 from einops import rearrange
 
+# Kernels
 
-### Kernels ###
+
 def gaussian_Q_half_kernel(
     landmarks: jnp.ndarray, alpha: float, sigma: float
 ) -> jnp.ndarray:
     xy_coords = landmarks.reshape(-1, 2)
-    num_landmarks = xy_coords.shape[0]
     diff = xy_coords[:, jnp.newaxis, :] - xy_coords[jnp.newaxis, :, :]
     dis = jnp.sum(jnp.square(diff), axis=-1)
     kernel = alpha * jnp.exp(-dis / sigma**2)
-    Q_half = jnp.einsum("ij,kl->ikjl", kernel, jnp.eye(2))
-    Q_half = Q_half.reshape(2 * num_landmarks, 2 * num_landmarks)
+    Q_half = jnp.kron(kernel, jnp.eye(2))
     return Q_half
 
 
-def gaussian_Q_kernel(
-    landmarks: jnp.ndarray, alpha: float, sigma: float
-) -> jnp.ndarray:
-    xy_coords = landmarks.reshape(-1, 2)
-    num_landmarks = xy_coords.shape[0]
-    diff = xy_coords[:, jnp.newaxis, :] - xy_coords[jnp.newaxis, :, :]
-    dis = jnp.sum(jnp.square(diff), axis=-1)
-    kernel = (
-        0.5 * (alpha**4) * (sigma**2) * jnp.pi * jnp.exp(-0.5 * dis / (sigma**2))
-    )
-    Q = jnp.einsum("ij,kl->ikjl", kernel, jnp.eye(2))
-    Q = Q.reshape(2 * num_landmarks, 2 * num_landmarks)
-    return Q
+def gaussian_kernel_2d(alpha: float, sigma: float) -> callable:
+    def k(x, y):
+        return alpha * jnp.exp(
+            -0.5 * jnp.sum(jnp.square(x - y), axis=-1) / (sigma**2)
+        )
+
+    return k
 
 
-def gaussian_kernel_2d(
-    x: jnp.ndarray, y: jnp.ndarray, alpha: float, sigma: float
-) -> jnp.ndarray:
-    return alpha * jnp.exp(-jnp.linalg.norm(x - y, axis=-1) ** 2 / (2 * sigma**2))
+def gaussian_kernel_independent(alpha, sigma, grid_range, grid_size, dim=2):
+    grid = jnp.linspace(*grid_range, grid_size)
+    grid = jnp.stack(jnp.meshgrid(grid, grid, indexing="xy"), axis=-1)
+    grid_ = grid.reshape(-1, dim)
+
+    gauss_kernel = gaussian_kernel_2d(alpha, sigma)
+    batch_over_grid = jax.vmap(gauss_kernel, in_axes=(None, 0))
+    batch_over_vals = jax.vmap(batch_over_grid, in_axes=(0, None))
+
+    def kernel(val):
+        Q_half = batch_over_vals(val, grid_)
+        Q_half = Q_half.reshape(-1, grid_size**2)
+        return Q_half
+
+    return kernel
 
 
 @dataclasses.dataclass
 class SDE:
     T: float
-    N: float
+    N: int
     dim: int
     n_bases: int
-    dt: float
-    ts: jax.Array
     drift: Callable
     diffusion: Callable
+    bm_shape: tuple
     params: Any
+
+    @property
+    def dt(self):
+        return self.T / self.N
+
+    @property
+    def ts(self):
+        return jnp.linspace(0, self.T, self.N)
 
     def covariance(self, val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
         """covariance term: sigma @ sigma^T"""
@@ -62,83 +72,87 @@ class SDE:
 
     def div_covariance(self, val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
         """divergence of covariance term: nabla_x (sigma @ sigma^T)"""
-        _jacobian = jax.jacfwd(self.covariance, argnums=0, holomorphic=True)(val, time)
-        return jnp.trace(_jacobian, axis1=-2, axis2=-1)
-
-    def reverse(self, score_fun):
-        def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
-            inverted_time = self.T - time
-            rev_drift_term = self.drift(val=val, time=inverted_time)
-
-            _score = jnp.squeeze(score_fun(val=val, time=inverted_time))
-            _covariance = self.covariance(val=val, time=inverted_time)
-            score_term = jnp.dot(_covariance, _score)
-
-            div_term = self.div_covariance(val=val, time=inverted_time)
-            return -rev_drift_term + score_term + div_term
-
-        def diffusion(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
-            inverted_time = self.T - time
-            return self.diffusion(val=val, time=inverted_time)
-
-        return SDE(
-            T=self.T,
-            N=self.N,
-            dim=self.dim,
-            n_bases=self.n_bases,
-            dt=self.dt,
-            ts=self.ts,
-            drift=drift,
-            diffusion=diffusion,
-            params=self.params,
-        )
-
-    def bridge_sde(self, score_func):
-        """Bridge the SDE using either pre-assigned score h or neural network approximation"""
-
-        def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
-            orig_drift_term = self.drift(val=val, time=time)
-
-            _score = jnp.squeeze(score_func(val=val, time=time))
-            _covariance = self.covariance(val=val, time=time)
-            score_term = jnp.dot(_covariance, _score)
-            return orig_drift_term + score_term
-
-        def diffusion(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
-            return self.diffusion(val=val, time=time)
-
-        return SDE(
-            T=self.T,
-            N=self.N,
-            n_bases=self.n_bases,
-            ts=self.ts,
-            drift=drift,
-            diffusion=diffusion,
-            params=self.params,
-        )
+        _jacobian = jax.jacfwd(self.covariance, argnums=0)(val, time)
+        # _jacobian = jax.jacfwd(self.covariance, argnums=0, holomorphic=True)(val, time)
+        # return jnp.trace(_jacobian, axis1=-2, axis2=-1)
+        return jnp.trace(_jacobian)
 
 
-def brownian_sde(T, N, dim, n_bases, dt, ts, alpha):
+def reverse(sde, score_fun):
+    def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
+        inverted_time = sde.T - time
+        rev_drift_term = sde.drift(val=val, time=inverted_time)
+
+        _score = jnp.squeeze(score_fun(val=val, time=inverted_time))
+        _covariance = sde.covariance(val=val, time=inverted_time)
+        score_term = jnp.dot(_covariance, _score)
+
+        div_term = sde.div_covariance(val=val, time=inverted_time)
+        return -rev_drift_term + score_term + div_term
+
+    def diffusion(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
+        inverted_time = sde.T - time
+        return sde.diffusion(val=val, time=inverted_time)
+
+    return SDE(
+        T=sde.T,
+        N=sde.N,
+        dim=sde.dim,
+        n_bases=sde.n_bases,
+        drift=drift,
+        diffusion=diffusion,
+        bm_shape=sde.bm_shape,
+        params=sde.params,
+    )
+
+
+def bridge(sde, score_fun):
+    """Bridge the SDE using either pre-assigned score h or neural network approximation"""
+
+    def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
+        orig_drift_term = sde.drift(val=val, time=time)
+
+        _score = jnp.squeeze(score_fun(val=val, time=time))
+        _covariance = sde.covariance(val=val, time=time)
+        score_term = jnp.dot(_covariance, _score)
+        return orig_drift_term + score_term
+
+    def diffusion(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
+        return sde.diffusion(val=val, time=time)
+
+    return SDE(
+        T=sde.T,
+        N=sde.N,
+        dim=sde.dim,
+        n_bases=sde.n_bases,
+        drift=drift,
+        diffusion=diffusion,
+        bm_shape=sde.bm_shape,
+        params=sde.params,
+    )
+
+
+def brownian_sde(T, N, dim, n_bases, sigma):
     def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
         return jnp.zeros_like(val)
 
     def diffusion(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
-        return alpha * jnp.eye(dim)
+        assert val.ndim == 1
+        return sigma * jnp.eye(val.size)
 
     return SDE(
         T=T,
         N=N,
         dim=dim,
         n_bases=n_bases,
-        dt=dt,
-        ts=ts,
         drift=drift,
         diffusion=diffusion,
+        bm_shape=(dim * n_bases,),
         params=None,
     )
 
 
-def damped_brownian_sde(T, N, dim, n_bases, dt, ts, alpha):
+def damped_brownian_sde(T, N, dim, n_bases, alpha):
     def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
         return jnp.zeros_like(val)
 
@@ -155,15 +169,14 @@ def damped_brownian_sde(T, N, dim, n_bases, dt, ts, alpha):
         N=N,
         dim=dim,
         n_bases=n_bases,
-        dt=dt,
-        ts=ts,
         drift=drift,
         diffusion=diffusion,
+        bm_shape=(dim * n_bases,),
         params=None,
     )
 
 
-def gaussian_kernel_sde(T, N, dim, n_bases, dt, ts, alpha, sigma):
+def gaussian_kernel_sde(T, N, dim, n_bases, alpha, sigma):
     def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
         return jnp.zeros_like(val)
 
@@ -175,16 +188,49 @@ def gaussian_kernel_sde(T, N, dim, n_bases, dt, ts, alpha, sigma):
         N=N,
         dim=dim,
         n_bases=n_bases,
-        dt=dt,
-        ts=ts,
         drift=drift,
         diffusion=diffusion,
+        bm_shape=(dim * n_bases,),
+        params=None,
+    )
+
+
+def gaussian_independent_kernel_sde(
+    T: float,
+    N: int,
+    dim: int,
+    n_bases: int,
+    alpha: float,
+    sigma: float,
+    grid_range: tuple,
+    grid_size: int,
+):
+    def drift(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
+        return jnp.zeros_like(val)
+
+    def diffusion(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
+        kernel = gaussian_kernel_independent(
+            alpha, sigma, grid_range=grid_range, grid_size=grid_size
+        )
+        val = val.reshape(-1, dim)
+        Q_half = kernel(val)
+        # Q_half = jnp.kron(Q_half, jnp.eye(2))
+        return Q_half
+
+    return SDE(
+        T=T,
+        N=N,
+        dim=dim,
+        n_bases=n_bases,
+        drift=drift,
+        diffusion=diffusion,
+        bm_shape=(grid_size**2, dim),
         params=None,
     )
 
 
 def fourier_gaussian_kernel_sde(
-    T, N, dim, n_bases, dt, ts, alpha, sigma, init_S, n_grid, grid_range, n_samples
+    T, N, dim, n_bases, alpha, sigma, init_S, n_grid, grid_range, n_samples
 ):
     n_padding = (n_samples - n_bases) // 2
 
@@ -222,12 +268,13 @@ def fourier_gaussian_kernel_sde(
         return X_eval
 
     def evaluate_Q(val: jnp.ndarray) -> jnp.ndarray:
+        kernel = gaussian_kernel_2d(alpha, sigma)
         X_eval = evaluate_X(val)
         S_eval = X_eval + evaluate_S(init_S)  # (n_bases, 2)
         Q_eval = jax.vmap(
             jax.vmap(
                 jax.vmap(
-                    partial(gaussian_kernel_2d, alpha=alpha, sigma=sigma),
+                    kernel,
                     in_axes=(None, 0),
                     out_axes=0,
                 ),
@@ -256,14 +303,10 @@ def fourier_gaussian_kernel_sde(
         return jnp.zeros_like(val)
 
     def diffusion(val: jnp.ndarray, time: jnp.ndarray) -> jnp.ndarray:
-        _diffusion_block = evaluate_diffusion(val)
-        _zero_block = jnp.zeros_like(_diffusion_block)
-        _diffusion = jnp.block(
-            [[_diffusion_block, _zero_block], [_zero_block, _diffusion_block]]
-        )
-        return _diffusion  # (2*n_bases, 2*n_grid**2)
+        diffusion_ = evaluate_diffusion(val)
+        return jnp.kron(jnp.eye(2), diffusion_)
 
-    return (SDE(T, N, dim, n_bases, dt, ts, drift, diffusion, params=None),)
+    return SDE(T, N, dim, n_bases, drift, diffusion, bm_shape=(grid.size,), params=None)
 
 
 # class FourierGaussianKernelSDE(SDE):
